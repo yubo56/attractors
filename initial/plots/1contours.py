fix signs I think? at least plots look right

initial/plots/1contours.py
@@ -4,54 +4,37 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
-from utils import roots
+from utils import roots, get_four_subplots, plot_point, H, get_grids
 
 def plot_H_for_eta(ax, eta, I=np.radians(20)):
     '''
     Hamiltonian is H = 1/2 cos^2(theta) + eta * sin(phi)
     canonical variables are (phi, x = cos(theta))
     '''
-    def H(x, phi):
-        return 0.5 * x**2 - eta * (
-            x * np.cos(I) -
-            np.sqrt(1 - x**2) * np.sin(I) * np.cos(phi))
-
-    _phi = np.linspace(0, 2 * np.pi, 50)
-    _x = np.linspace(-1, 1, 50)
-    phi_grid = np.outer(_phi, np.ones_like(_x))
-    x_grid = np.outer(np.ones_like(_phi), _x)
-    H_grid = H(x_grid, phi_grid)
+    x_grid, phi_grid = get_grids()
+    H_grid = H(I, eta, x_grid, phi_grid)
     ax.contour(phi_grid, x_grid, H_grid, cmap='RdBu_r', linewidths=0.8)
 
-    thetas, phis = roots(eta, I)
-    colors = ['b', 'g', 'r', 'y'] if len(thetas) == 4 else ['b', 'y']
+    thetas, phis = roots(I, eta)
+    colors = ['r', 'm', 'g', 'c'] if len(thetas) == 4 else ['m', 'g']
     for color, theta, phi in zip(colors, thetas, phis):
-        phi_arr = [phi] if theta > 0 else [phi, phi + 2 * np.pi]
-        for phi_plot in phi_arr:
-            ax.plot(phi_plot, np.cos(theta), '%so' % color, markersize=6)
+        plot_point(ax, theta, '%so' % color, markersize=6)
+
     ax.set_title(r'(I, $\eta$)=($%d^\circ$, %.3f)' % (np.degrees(I), eta),
                  fontsize=8)
-    ax.set_xticks([0, np.pi, 2 * np.pi])
 
     if len(thetas) == 4:
-        # plot separatrix, which is H at q4
         ax.contour(phi_grid,
                    x_grid,
                    H_grid,
-                   levels=[H(np.cos(thetas[3]), phis[3])],
+                   levels=[H(I, eta, np.cos(thetas[3]), phis[3])],
                    colors=['k'],
                    linewidths=1.6)
 
 if __name__ == '__main__':
-    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex=True, sharey=True)
-    f.subplots_adjust(wspace=0)
+    f, axs = get_four_subplots()
     # Figures 3b-3e of Kassandra's paper
-    for ax, eta in zip([ax1, ax2, ax3, ax4], [0.1, 0.5, 0.561, 2]):
+    for ax, eta in zip(axs, [0.1, 0.5, 0.561, 2]):
         plot_H_for_eta(ax, eta)
-    ax1.set_ylabel(r'$\cos \theta$')
-    ax3.set_xlabel(r'$\phi$')
-    ax3.set_xticklabels(['0', r'$\pi$', r'$2\pi$'])
-    ax3.set_ylabel(r'$\cos \theta$')
-    ax4.set_xlabel(r'$\phi$')
+
     plt.savefig('1contours.png', dpi=400)
-    plt.clf()

initial/plots/2evolution.py
@@ -4,46 +4,38 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif', size=14)
-from utils import roots, solve_ic
+from utils import roots, solve_ic, to_cart, to_ang, get_four_subplots,\
+    plot_point, get_phi
 
 if __name__ == '__main__':
     eta = 0.1
-    tide = 0.01
+    tide = 0.001
     I = np.radians(20)
     T_F = 10000
 
-    qs, phis = roots(eta, I)
+    qs, phis = roots(I, eta)
 
     pert = 0.08 # perturbation strength
 
-    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex=True, sharey=True)
-    f.subplots_adjust(wspace=0)
+    f, axs = get_four_subplots()
 
-    for q0, phi0, ax in zip(qs, phis, [ax1, ax2, ax3, ax4]):
+    for q0, phi0, ax in zip(qs, phis, axs):
         q_i = q0 + pert
         phi_i = phi0 - pert
 
-        s0 = [
-            -np.sin(q_i) * np.cos(phi_i),
-            -np.sin(q_i) * np.sin(phi_i),
-            np.cos(q_i)]
-        sim_time, t, sol = solve_ic(I, eta, tide, s0, T_F, rtol=1e-6)
+        s0 = to_cart(q_i, phi_i)
+        sim_time, t, sol = solve_ic(I, eta, tide, s0, T_F)
         print('Sim time:', sim_time)
 
         x, y, z = sol
-        r = np.sqrt(x**2 + y**2 + z**2)
-        q = np.arccos(z / r) * np.sign(q_i)
-        phi = (np.arctan2(-y / np.sin(q), -x / np.sin(q)) + 2 * np.pi)\
-            % (2 * np.pi)
+        q, phi = to_ang(x, y, z)
+        phi = np.array([get_phi(th, phi=f) for th, f in zip(q, phi)])
 
         ax.plot(phi % (2 * np.pi),
                 np.cos(q),
                 'bo',
                 markersize=0.3)
-        ax.plot(phi0 % (2 * np.pi),
-                np.cos(q0),
-                'ro',
-                markersize=4)
+        plot_point(ax, q0, 'ro', markersize=4)
 
         ax.set_title(r'Init: $(\phi_0, \theta_0) = (%.3f, %.3f)$'
                      % (phi0, q0), fontsize=8)
@@ -51,9 +43,4 @@ if __name__ == '__main__':
 
     plt.suptitle(r'(I, $\eta$)=($%d^\circ$, %.3f)' % (np.degrees(I), eta),
                  fontsize=10)
-    ax1.set_ylabel(r'$\cos \theta$')
-    ax3.set_xlabel(r'$\phi$')
-    ax3.set_xticklabels(['0', r'$\pi$', r'$2\pi$'])
-    ax3.set_ylabel(r'$\cos \theta$')
-    ax4.set_xlabel(r'$\phi$')
     plt.savefig('2evolution.png', dpi=400)

initial/plots/utils.py
@@ -2,11 +2,26 @@ import numpy as np
 import scipy.optimize as opt
 from scipy.integrate import solve_ivp
 import time
+import matplotlib.pyplot as plt
 
-def get_phis(thetas):
-    return [np.pi if q > 0 else 0 for q in thetas]
+def to_cart(q, phi):
+    return [
+        np.sin(q) * np.cos(phi),
+        np.sin(q) * np.sin(phi),
+        np.cos(q),
+    ]
 
-def roots(eta, I):
+def to_ang(x, y, z):
+    r = np.sqrt(x**2 + y**2 + z**2)
+    q = np.arccos(z / r)
+    phi = (np.arctan2(y / np.sin(q), x / np.sin(q)) + 2 * np.pi)\
+        % (2 * np.pi)
+    return q, phi
+
+def get_phi(q, phi=0):
+    return phi + np.pi if q > 0 else phi
+
+def roots(I, eta):
     ''' returns theta roots from EOM '''
     eta_c = (np.sin(I)**(2/3) + np.cos(I)**(2/3))**(-3/2)
 
@@ -19,14 +34,14 @@ def roots(eta, I):
         inits = [0, np.pi / 2, -np.pi, -np.pi / 2]
         for qi in inits:
             roots.append(opt.newton(f, qi, fprime=fp))
-        return np.array(roots), get_phis(roots)
+        return np.array(roots), np.zeros_like(roots)
 
     else:
         roots = []
         inits = [np.pi / 2 - I, -np.pi + I]
         for qi in inits:
             roots.append(opt.newton(f, qi, fprime=fp))
-        return np.array(roots), get_phis(roots)
+        return np.array(roots), np.zeros_like(roots)
 
 def get_dydt(I, eta, tide):
     ''' get dy/dt for params '''
@@ -50,7 +65,7 @@ def get_jac(I, eta, tide):
         ]
     return jac
 
-def solve_ic(I, eta, tide, y0, tf, method='RK45', **kwargs):
+def solve_ic(I, eta, tide, y0, tf, method='RK45', rtol=1e-6, **kwargs):
     '''
     wraps solve_ivp and returns sim time
     '''
@@ -58,7 +73,45 @@ def solve_ic(I, eta, tide, y0, tf, method='RK45', **kwargs):
     dydt = get_dydt(I, eta, tide)
     jac = get_jac(I, eta, tide)
     if 'RK' in method:
-        ret = solve_ivp(dydt, [0, tf], y0, method=method, **kwargs)
+        ret = solve_ivp(dydt, [0, tf], y0,
+                        rtol=rtol, method=method, **kwargs)
     else:
-        ret = solve_ivp(dydt, [0, tf], y0, method=method, jac=jac, **kwargs)
+        ret = solve_ivp(dydt, [0, tf], y0,
+                        rtol=rtol, method=method, jac=jac, **kwargs)
     return time.time() - time_i, ret.t, ret.y
+
+def get_four_subplots():
+    ''' keep using four subplots w/ same settings '''
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex=True, sharey=True)
+    axs = [ax1, ax2, ax3, ax4]
+    for ax in axs:
+        ax.set_xlim([0, 2 * np.pi])
+        ax.set_ylim([-1, 1])
+    f.subplots_adjust(wspace=0.07)
+    ax1.set_ylabel(r'$\cos \theta$')
+    ax3.set_xlabel(r'$\phi$')
+    ax.set_xticks([0, np.pi, 2 * np.pi])
+    ax3.set_xticklabels(['0', r'$\pi$', r'$2\pi$'])
+    ax3.set_ylabel(r'$\cos \theta$')
+    ax4.set_xlabel(r'$\phi$')
+
+    return f, axs
+
+def plot_point(ax, q, *args, **kwargs):
+    ''' plot Cassini state including wrap around logic '''
+    phi = get_phi(q)
+    phi_arr = [phi] if abs(phi % (2 * np.pi)) > 0.5 else [phi, phi + 2 * np.pi]
+    for phi_plot in phi_arr:
+        ax.plot(phi_plot, np.cos(q), *args, **kwargs)
+
+def H(I, eta, x, phi):
+    return 0.5 * x**2 - eta * (
+        x * np.cos(I) -
+        np.sqrt(1 - x**2) * np.sin(I) * np.cos(phi))
+
+def get_grids(N=50):
+    _phi = np.linspace(0, 2 * np.pi, N)
+    _x = np.linspace(-1, 1, N)
+    phi_grid = np.outer(_phi, np.ones_like(_x))
+    x_grid = np.outer(np.ones_like(_phi), _x)
+    return x_grid, phi_grid