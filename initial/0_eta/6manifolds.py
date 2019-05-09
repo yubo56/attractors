d7e83d96baecf43f598df54a33e5d5432d58eb4d
Yubo Su <yubo@blend.com>
2019-05-09 17:04:43 -0400

    okay get Phop correctly

--- initial/0_eta/6manifolds.py
+++ initial/0_eta/6manifolds.py
@@ -37,8 +37,7 @@ def plot_manifolds(eta):
     q1, _phi1 = to_ang(*s1)
     phi1 = np.unwrap(_phi1 + np.pi) - 2 * np.pi
     term1 = np.where(phi1 < 0)[0][0]
-    plt.plot(phi1[ :term1], s1[2, :term1],
-             label=r'$W_s^{(0)}(%.2f)$' % abs(t1[term1]))
+    plt.plot(phi1[ :term1], s1[2, :term1], label=r'$W_s^{(0)}$', linewidth=0.6)
 
     # forwards from CS4^0
     init = get_displaced(-1, 1)
@@ -47,11 +46,10 @@ def plot_manifolds(eta):
     phi2 = np.unwrap(_phi2 + np.pi) - 2 * np.pi
     phi2_grad = np.gradient(phi2) / np.gradient(t2)
     term2 = np.where(np.logical_and(
-        abs(phi2_grad) < 2 * min(abs(phi2_grad)),
+        abs(phi2_grad) < 5 * min(abs(phi2_grad)),
         phi2 < 1,
     ))[0][0]
-    plt.plot(phi2[ :term2], s2[2, :term2],
-             label=r'$W_u^{(0)}(%.2f)$' % abs(t2[term2]))
+    plt.plot(phi2[ :term2], s2[2, :term2], label=r'$W_u^{(0)}$', linewidth=0.6)
 
     # backwards from CS4^1
     init = get_displaced(-1, -1)
@@ -59,8 +57,7 @@ def plot_manifolds(eta):
     q3, _phi3 = to_ang(*s3)
     phi3 = np.unwrap(_phi3 + np.pi)
     term3 = np.where(phi3 < 0)[0][0]
-    plt.plot(phi3[ :term3], s3[2, :term3],
-             label=r'$W_s^{(1)}(%.2f)$' % abs(t3[term3]))
+    plt.plot(phi3[ :term3], s3[2, :term3], label=r'$W_s^{(1)}$', linewidth=0.6)
 
     # forwards from CS4^1
     init = get_displaced(1, -1)
@@ -68,20 +65,20 @@ def plot_manifolds(eta):
     q4, _phi4 = to_ang(*s4)
     phi4 = np.unwrap(_phi4 + np.pi)
     term4 = np.where(phi4 < 0)[0][0]
-    plt.plot(phi4[ :term4], s4[2, :term4],
-             label=r'$W_u^{(1)}(%.2f)$' % abs(t4[term4]))
+    plt.plot(phi4[ :term4], s4[2, :term4], label=r'$W_u^{(1)}$', linewidth=0.6)
 
     plt.xlim([0, 2 * np.pi])
     mu1, mu2, mu3 = min(s1[2, :term1]), min(s2[2, :term2]), min(s3[2, :term3])
-    mu1max, mu4max = max(s1[2, :term1]), max(s4[2, :term4])
+    mu1max, mu2max, mu4max = max(s1[2, :term1]), max(s2[2, :term2]), max(s4[2, :term4])
 
     plt.xlabel(r'$\phi$')
     plt.ylabel(r'$\cos \theta$')
-    plt.title(r'$\eta = %.1f, \Delta \cos \theta = (%.3f, %.3f, %.3f)$' %
-              (eta, (mu2 - mu1) / (mu2 - mu3), mu4max - mu1max, mu2 - mu3))
+    plt.title(r'$\eta = %.1f, \Delta \cos \theta = (%.4f, %.3f), P = %.3f$' %
+              (eta, mu2 - mu1 , mu2 - mu3, (mu2 - mu1) / (mu2 - mu3)))
 
     plt.legend()
-    plt.savefig('6manifolds%s.png' % ('%.2f' % eta).replace('.', '_'))
+    plt.savefig('6manifolds%s.png' % ('%.2f' % eta).replace('.', '_'),
+                dpi=400)
     plt.clf()
 
 
