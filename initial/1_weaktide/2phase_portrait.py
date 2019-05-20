1da9609ed388844ea5b97c983243aba858cda770
Yubo Su <yubo@blend.com>
2019-05-20 17:31:33 -0400

    check CS4 +- cass_width cross section

--- initial/1_weaktide/2phase_portrait.py
+++ initial/1_weaktide/2phase_portrait.py
@@ -19,15 +19,20 @@ def plot_portrait(I=np.radians(20), s_c=1.5):
     eps = 1e-4
     _mu = np.linspace(-1, 1, N_PTS)
     _s = np.linspace(0.5, smax, N_PTS)
-    s2 = np.linspace(2, smax, N_PTS)
     quiv_scale = 400
+
+    # for integrated flow portraits, mu bound where mu4 disappears
+    _mu_int = np.linspace(-0.95, 1, N_PTS) # flow is really strong near (2, -1)
+    mu4_int = get_mu4(I, s_c, _s)
+    idx4_int = np.where(mu4_int > 0)[0]
+    s_int, mu_int = np.meshgrid(_s[idx4_int][::2], _mu_int[::2])
 
     def plot_shareds(s_arr=_s, plot_all=True):
         ''' few lines are shared on phase portrait and quiver'''
 
         if plot_all:
             # plot where dmu changes signs
-            plt.plot(s2, 2 / s2, 'r', label=r'$d\mu = 0$')
+            plt.plot(_s, 2 / _s, 'r', label=r'$d\mu = 0$')
 
             # plot bounding (t -> -\infty, mu = 0, s = infty)
             mu, s, _ = get_inf_avg_sol(smax)
@@ -50,7 +55,7 @@ def plot_portrait(I=np.radians(20), s_c=1.5):
         plt.ylabel(r'$\mu$')
         plt.title(r'$s_c=%.1f$ ($\mu(\phi) = \mu(0)$)' % s_c)
         mu4, idx4 = plot_shareds()
-        cass_width = np.sqrt(s_c / s2[idx4] * np.cos(I))
+        cass_width = np.sqrt(s_c / _s[idx4] * np.cos(I))
         plt.fill_between(_s[idx4],
                          mu4[idx4] + cass_width, mu4[idx4] - cass_width,
                          color='m', alpha=0.3)
@@ -80,28 +85,8 @@ def plot_portrait(I=np.radians(20), s_c=1.5):
         plt.xlim(0, smax)
         plt.ylim(-1.1, 1.1)
         plt.legend(loc='lower right')
-        plt.title(r'$s_c=%.1f$' % s_c)
+        plt.title(r'$s_c=%.1f$ ($\mu(\phi) = \mu(0)$)' % s_c)
         plt.savefig('2phase_portrait%s.png' % stringify(s_c), dpi=400)
-        plt.clf()
-
-    # phase portrait w/ under precession-integrated approximation
-    _mu_int = np.linspace(-0.95, 1, N_PTS) # flow is really strong near (2, -1)
-    s_int, mu_int = np.meshgrid(s2[::2], _mu_int[::2])
-    def phase_int():
-        dydt_num_avg = get_dydt_num_avg(I, s_c, eps)
-        ds_num_avg, dmu_num_avg = dydt_num_avg(s_int, [mu_int])
-        plt.quiver(s_int, mu_int, ds_num_avg, dmu_num_avg, scale=quiv_scale)
-        mu4, idx4 = plot_shareds(s2, False) # just plot CS4 for clarity
-        plt.xlabel(r'$s$')
-        plt.ylabel(r'$\cos\theta$')
-        plt.xlim(0, smax)
-        plt.ylim(-1.1, 1.1)
-        plt.title(r'$s_c=%.1f$ (Integrated)' % s_c)
-        cass_width = np.sqrt(s_c / s2[idx4] * np.cos(I))
-        plt.fill_between(s2[idx4],
-                         mu4[idx4] + cass_width, mu4[idx4] - cass_width,
-                         color='m', alpha=0.3)
-        plt.savefig('2quiver_int%s.png' % stringify(s_c), dpi=400)
         plt.clf()
 
     # phase portrait w/ under precession-integrated approximation
@@ -109,23 +94,80 @@ def plot_portrait(I=np.radians(20), s_c=1.5):
         dydt_piecewise = get_dydt_piecewise(I, s_c)
         ds_piecewise, dmu_piecewise = dydt_piecewise(s_int, [mu_int])
         plt.quiver(s_int, mu_int, ds_piecewise, dmu_piecewise, scale=quiv_scale)
-        mu4, idx4 = plot_shareds(s2, False) # just plot CS4 for clarity
+        mu4, idx4 = plot_shareds(_s, False) # just plot CS4 for clarity
         plt.xlabel(r'$s$')
         plt.ylabel(r'$\cos\theta$')
         plt.xlim(0, smax)
         plt.ylim(-1.1, 1.1)
         plt.title(r'$s_c=%.1f$ (Approximate)' % s_c)
-        cass_width = np.sqrt(s_c / s2[idx4] * np.cos(I))
-        plt.fill_between(s2[idx4],
+        cass_width = np.sqrt(s_c / _s[idx4] * np.cos(I))
+        plt.fill_between(_s[idx4],
                          mu4[idx4] + cass_width, mu4[idx4] - cass_width,
                          color='m', alpha=0.3)
         plt.savefig('2quiver_pw%s.png' % stringify(s_c), dpi=400)
         plt.clf()
 
+    # cross section with an effective width guessed from piecewise
+    def cross_pw():
+        mu4, idx4 = plot_shareds(_s) # just plot CS4 for clarity
+        cass_width = np.sqrt(s_c / _s * np.cos(I))
+
+        sols_top = []
+        sols_bot = []
+        bounds = []
+        for idx in idx4:
+            s_curr = _s[idx]
+            ret_top = solve_ivp(dmu_ds_nocs,
+                                [s_curr, smax],
+                                [mu4[idx] + cass_width[idx]],
+                                max_step=0.1,
+                                dense_output=True)
+            sols_top.append(ret_top.sol)
+            sols_top_curr = [sol(s_curr) for sol in sols_top]
+            ret_bot = solve_ivp(dmu_ds_nocs,
+                                [s_curr, smax],
+                                [mu4[idx] - cass_width[idx]],
+                                max_step=0.1,
+                                dense_output=True)
+            sols_bot.append(ret_bot.sol)
+            sols_bot_curr = [sol(s_curr) for sol in sols_bot]
+
+            bounds.append([np.max(sols_top_curr), np.min(sols_bot_curr)])
+        mu4_min, mu4_max = np.array(bounds).T
+        plt.fill_between(_s[idx4], mu4_min, mu4_max, color='g', alpha=0.3)
+
+        plt.xlabel(r'$s$')
+        plt.ylabel(r'$\cos\theta$')
+        plt.xlim(0, smax)
+        plt.ylim(-1.1, 1.1)
+        plt.legend(loc='lower right')
+        plt.title(r'$s_c=%.1f$ (Piecewise)' % s_c)
+        plt.savefig('2phase_portrait_pw%s.png' % stringify(s_c), dpi=400)
+        plt.clf()
+
+    # phase portrait w/ under precession-integrated approximation
+    def phase_int():
+        dydt_num_avg = get_dydt_num_avg(I, s_c, eps)
+        ds_num_avg, dmu_num_avg = dydt_num_avg(s_int, [mu_int])
+        plt.quiver(s_int, mu_int, ds_num_avg, dmu_num_avg, scale=quiv_scale)
+        mu4, idx4 = plot_shareds(_s, False) # just plot CS4 for clarity
+        plt.xlabel(r'$s$')
+        plt.ylabel(r'$\cos\theta$')
+        plt.xlim(0, smax)
+        plt.ylim(-1.1, 1.1)
+        plt.title(r'$s_c=%.1f$ (Integrated)' % s_c)
+        cass_width = np.sqrt(s_c / _s[idx4] * np.cos(I))
+        plt.fill_between(_s[idx4],
+                         mu4[idx4] + cass_width, mu4[idx4] - cass_width,
+                         color='m', alpha=0.3)
+        plt.savefig('2quiver_int%s.png' % stringify(s_c), dpi=400)
+        plt.clf()
+
     phase_nocs()
     cross_nocs()
-    phase_int()
     phase_pw()
+    cross_pw()
+    phase_int()
 
 if __name__ == '__main__':
     # plot_portrait()
