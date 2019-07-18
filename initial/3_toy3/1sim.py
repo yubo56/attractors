74df379d4c8e461f8813bb4cb67f2bb5f67080e3
Yubo Su <yubo@blend.com>
2019-07-18 16:12:27 -0400

    toy problem 3 seems to have plausible agreement

---
+++ initial/3_toy3/1sim.py
@@ -0,0 +1,120 @@
+import os
+import scipy.optimize as opt
+import numpy as np
+import multiprocessing as mp
+
+import matplotlib
+import pickle
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size=12)
+
+PLOT_DIR = '1plots'
+from utils import to_cart, to_ang, solve_ic, get_areas, get_plot_coords,\
+    roots, H, get_mu4
+
+EPS = 3e-4
+
+def my_fmt(I_deg, delta):
+    return '%d_%s' % (I_deg, ('%.2f' % delta).replace('.', '_'))
+
+def run_single(I, tf, eta0, q0, delta):
+    q4 = roots(I, eta0)[3]
+
+    y0 = [*to_cart(q0, 0.2), eta0]
+    return solve_ic(I, EPS, delta, y0, tf)
+
+def stats_runner(I, q0, delta):
+    tf = 3000
+    n_eta = 151
+
+    def result_single(tf, eta0):
+        ret = run_single(I, tf, eta0, q0, delta)
+
+        q_f, phi_f = to_ang(ret.y[0][-1], ret.y[1][-1], ret.y[2][-1])
+        eta_f = ret.y[3, -1]
+        q4 = roots(I, eta_f)[3]
+        dH_f = H(I, eta_f, q_f, phi_f) - H(I, eta_f, q4, 0)
+
+        _, _, t_cross, _ = get_areas(ret)
+        crossed_idxs = np.where(ret.t > t_cross)[0]
+        if len(crossed_idxs) == 0:
+            return dH_f > 0, -1
+        return dH_f > 0, ret.y[3, crossed_idxs[0]], ret.y[2, -1]
+
+    counts = 0
+    res = []
+    for eta0 in np.linspace(0.1, 0.2, n_eta):
+        capture, eta_f, z_f = result_single(tf, eta0)
+        print('\t', '%.3f' % q0, 'Finished', '%.3f' % (eta_f - eta0), capture,
+              '%.3f' % z_f)
+        res.append((eta_f, capture))
+        if capture:
+                counts += 1
+    return res
+
+def run_stats(I_deg, delta, p=None):
+    I = np.radians(I_deg)
+    n_q = 151
+
+    PKL_FN = '1dat%s.pkl' % my_fmt(I_deg, delta)
+    if not os.path.exists(PKL_FN):
+        q_vals = -np.pi / 2 - np.linspace(0.1, 0.3, n_q)
+
+        if p:
+            res_arr = p.starmap(stats_runner, [(I, q0, delta) for q0 in q_vals])
+        else:
+            res_arr = [stats_runner(I, q0, delta) for q0 in q_vals]
+        with open(PKL_FN, 'wb') as f:
+            pickle.dump(res_arr, f)
+    else:
+        with open(PKL_FN, 'rb') as f:
+            res_arr = pickle.load(f)
+
+    captures, escapes = [], []
+    for res in res_arr:
+        for etaf, capture in res:
+            if capture:
+                captures.append(etaf)
+            else:
+                escapes.append(etaf)
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    fig.subplots_adjust(hspace=0)
+    n, bins, _ = ax1.hist([captures, escapes],
+                           bins=40,
+                           label=['Capture', 'Escape'],
+                           stacked= True)
+    ax2.set_xlabel(r'$\eta_\star$')
+    ax1.legend()
+
+    eta_vals = (bins[ :-1] + bins[1: ]) / 2
+    # ax2.plot(eta_vals, n[0] / n[1])
+    nonzero_idxs = np.where(n[1] != 0)[0]
+    ax2.errorbar(eta_vals[nonzero_idxs],
+                 n[0][nonzero_idxs] / n[1][nonzero_idxs],
+                 yerr = np.sqrt(n[0][nonzero_idxs]) / n[1][nonzero_idxs],
+                 fmt='o', label='Data')
+
+    # overplot fit
+    def fit(eta):
+        ''' P_hop(eta) analytical from bottom'''
+        return (
+            (32 * np.cos(I) * eta + 8 * delta) * np.sqrt(eta * np.sin(I))
+        ) / (
+            2 * np.pi * (1 - 2 * eta * np.cos(I) + delta * eta * np.cos(I))
+                + (16 * np.cos(I) * eta + 4 * delta) * np.sqrt(eta * np.sin(I)))
+    ax2.plot(eta_vals, fit(eta_vals), 'r', linewidth=2, label='Analytical')
+    ax1.set_title(r'$I = %d^\circ$' % I_deg)
+    ax2.set_ylabel('Capture Probability')
+    ax1.set_ylabel('Counts')
+    ax2.legend()
+    plt.savefig('1hist%s.png' % my_fmt(I_deg, delta),
+                dpi=400)
+    plt.close(fig)
+
+if __name__ == '__main__':
+    p = mp.Pool(4)
+    for delta in [0, 0.3, 0.5, 0.7]:
+        run_stats(20, delta, p)
---
+++ initial/3_toy3/utils.py
@@ -0,0 +1,182 @@
+'''
+utils functions for toy problem 2
+
+since we're only concerned w/ separatrix hopping cases, we will ignore ICs
+inside the separatrix or that start too close to CS1/CS3
+'''
+import numpy as np
+import scipy.optimize as opt
+from scipy.integrate import solve_ivp
+
+########
+# COPIED
+########
+
+def to_cart(q, phi):
+    return [
+        np.sin(q) * np.cos(phi),
+        np.sin(q) * np.sin(phi),
+        np.cos(q),
+    ]
+
+def to_ang(x, y, z):
+    r = np.sqrt(x**2 + y**2 + z**2)
+    q = np.arccos(z / r)
+    phi = (np.arctan2(y / np.sin(q), x / np.sin(q)) + np.pi)\
+        % (2 * np.pi)
+    return q, phi
+
+def get_etac(I):
+    return (np.sin(I)**(2/3) + np.cos(I)**(2/3))**(-3/2)
+
+def roots(I, eta):
+    '''
+    returns theta roots from EOM, not phi (otherwise same as 0_eta func)
+    '''
+    eta_c = get_etac(I)
+
+    # function to minimize and derivatives
+    f = lambda q: -eta * np.sin(q - I) + np.sin(q) * np.cos(q)
+    fp = lambda q: -eta * np.cos(q - I) + np.cos(2 * q)
+
+    if eta < eta_c:
+        roots = []
+        inits = [0, np.pi / 2, -np.pi, -np.pi / 2]
+        for qi in inits:
+            roots.append(opt.newton(f, qi, fprime=fp))
+        return np.array(roots)
+
+    else:
+        roots = []
+        inits = [np.pi / 2 - I, -np.pi + I]
+        for qi in inits:
+            roots.append(opt.newton(f, qi, fprime=fp))
+        return np.array(roots)
+
+def H(I, eta, q, phi):
+    mu = np.cos(q)
+    return -0.5 * mu**2 + eta * (
+        mu * np.cos(I) -
+        np.sqrt(1 - mu**2) * np.sin(I) * np.cos(phi))
+
+def get_mu4(I, eta):
+    '''
+    gets mu4 for a list of spins s, returning -1 if no CS4 for given s
+    '''
+    eta_c = get_etac(I)
+    mu4 = np.full(np.shape(eta), -1.0)
+
+    valid_idxs = np.where(eta < eta_c)
+
+    for idx in zip(*valid_idxs):
+        mu4[idx] = np.cos(roots(I, eta[idx])[3])
+
+    return mu4
+
+############
+# END COPIED
+############
+
+def get_areas(ret):
+    ''''
+    compute enclosed phase space area at time intervals. passed sol
+    interpolates (x, y, z, eta)
+
+    assume solutions only librate about CS2, and never begin @ libration
+    (assumptions detailed at top of file). then at all times, either t_0/t_pi
+    interlocking or only t_pi exists
+    - circulating: area is sum(-mu * dphi) between successive t_0 points
+    - librating: area is sum(-mu * dphi) between over *three* t_pi points
+    '''
+    num_pts = 100
+
+    [t_events] = ret.t_events
+    x_events, _, z_events, eta_events = ret.sol(t_events)
+    # phi = 0 means x < 0
+    idx_0 = np.where(x_events < 0)[0]
+    idx_pi = np.where(x_events > 0)[0]
+    t_0 = t_events[idx_0]
+    t_pi = t_events[idx_pi]
+
+    t_cross = np.inf
+    ends_circ = False
+
+    t_areas = []
+    areas = []
+
+    # NB: t_pi is always longer than t_0
+    for t_0_i, t_0_f, t_pi_i, t_pi_f in zip(t_0[ :-1], t_0[1: ],
+                                            t_pi, t_pi[1: ]):
+        # all t_0 points correspond to circulation
+        t_vals = np.linspace(t_0_i, t_0_f, num_pts)
+        sol_vals = np.array(ret.sol(t_vals)[ : 3]).T # index by time then var
+
+        t_areas.append((t_0_i + t_0_f) / 2)
+        # TODO maybe better integral?
+        area = 0
+        angs = [to_ang(*sol_val) for sol_val in sol_vals]
+        for (q0, phi0), (q1, phi1) in zip(angs[ :-1], angs[1: ]):
+            dphi = phi1 - phi0
+            if abs(dphi) > np.pi:
+                # can get wraparound effects, do this dumb way
+                dphi += -np.sign(dphi) * 2 * np.pi
+            area += -(np.cos(q1) + np.cos(q0)) / 2 * dphi
+
+        areas.append(area)
+
+        # detect escape
+        sol_at_i_f = ret.sol(np.array([t_0_i, t_0_f, t_pi_i, t_pi_f]))
+        mu_0_i, mu_0_f, mu_pi_i, mu_pi_f = sol_at_i_f[2]
+        if np.sign(mu_pi_f - mu_0_f) != np.sign(mu_pi_i - mu_0_i):
+            ends_circ = True
+            t_cross = (t_pi_f + t_pi_i) / 2
+            # debug how much adiabaticity is violated during crossing
+            # print('Etas during escape', sol_at_i_f[3])
+
+    _lib_ts = t_pi[np.where(t_pi > t_0[-1])[0]]
+    if len(_lib_ts) > 1:
+        t_cross = t_0[-1]
+
+        lib_ts = _lib_ts[ ::2] # only every other time
+        for t_0_i, t_0_f in zip(lib_ts[ :-1], lib_ts[1: ]):
+            t_vals = np.linspace(t_0_i, t_0_f, num_pts)
+            sol_vals = np.array(ret.sol(t_vals)[ : 3]).T
+
+            t_areas.append((t_0_i + t_0_f) / 2)
+            # TODO also better integral
+            area = 0
+            angs = [to_ang(*sol_val) for sol_val in sol_vals]
+            for (q0, phi0), (q1, phi1) in zip(angs[ :-1], angs[1: ]):
+                dphi = phi1 - phi0
+                if abs(dphi) > np.pi:
+                    # can get wraparound effects, do this dumb way
+                    dphi += -np.sign(dphi) * 2 * np.pi
+                area += -(np.cos(q1) + np.cos(q0)) / 2 * dphi
+
+            areas.append(area)
+
+    return t_areas, np.array(areas), t_cross, ends_circ
+
+def solve_ic(I, eps, delta, y0, tf, rtol=1e-6, **kwargs):
+    '''
+    get dy/dt for (x, y, z, eta)
+
+    returns (t, y, sep areas, ret)
+    '''
+    def dydt(t, s):
+        x, y, z, eta = s
+        return [
+            y * z - eta * y * np.cos(I) - eps * z * x,
+            -x * z + eta * (x * np.cos(I) - z * np.sin(I)) - eps * z * y,
+            eta * y * np.sin(I) + eps * (1 - z**2),
+            delta * eps * eta,
+        ]
+    event = lambda t, y: y[1]
+    ret = solve_ivp(dydt, [0, tf], y0,
+                    rtol=rtol, dense_output=True, events=[event],
+                    **kwargs)
+
+    return ret
+
+def get_plot_coords(q, phi):
+    return np.sin(q / 2) * np.cos(phi), np.sin(q / 2) * np.sin(phi)
