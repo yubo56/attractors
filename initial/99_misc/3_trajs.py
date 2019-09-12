51a48a89c43696831e585864c67b3b59e0e6015d
Yubo Su <yubo@blend.com>
2019-09-12 12:00:41 -0400

    can plot trajs for eta too

--- initial/99_misc/3_trajs.py
+++ initial/99_misc/3_trajs.py
@@ -1,5 +1,6 @@
 '''
-plot various trajectories in phase space
+plot 2pi trajectories as eps in weak tides is tuned up
+also can do for deta/dt case, though a bit hacky
 '''
 from scipy.integrate import solve_ivp
 import numpy as np
@@ -10,17 +11,6 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif', size=12)
-
-def get_dydt(I, eta, eps):
-    ''' get dy/dt for params '''
-    def dydt(t, s):
-        x, y, z = s
-        return [
-            y * z - eta * y * np.cos(I) - eps * z * x,
-            -x * z + eta * (x * np.cos(I) - z * np.sin(I)) - eps * z * y,
-            eta * y * np.sin(I) + eps * (1 - z**2),
-        ]
-    return dydt
 
 def to_ang(x, y, z):
     r = np.sqrt(x**2 + y**2 + z**2)
@@ -36,11 +26,38 @@ def to_cart(q, phi):
         np.cos(q),
     ]
 
-def trajs(I, eta, eps_mult=0, tf=300, n_pts=101, n_t=101, phi_c=None):
+def get_dydt(I, eta, eps):
+    ''' get dy/dt for params '''
+    def dydt(t, s):
+        x, y, z = s
+        return [
+            y * z - eta * y * np.cos(I) - eps * z * x,
+            -x * z + eta * (x * np.cos(I) - z * np.sin(I)) - eps * z * y,
+            eta * y * np.sin(I) + eps * (1 - z**2),
+        ]
+    return dydt
+
+def get_dydt_eta(I, eta, eps):
+    ''' get dy/dt for params '''
+    def dydt(t, s):
+        x, y, z, eta = s
+        return [
+            y * z - eta * y * np.cos(I),
+            -x * z + eta * (x * np.cos(I) - z * np.sin(I)),
+            eta * y * np.sin(I),
+            eps * eta,
+        ]
+    return dydt
+
+def trajs(I, eta, eps_mult=0, tf=150, n_pts=101, n_t=101, phi_c=None,
+          get_dydt=get_dydt, get_y0=None, fn_base='trajs'):
     '''
     for a range of ICs, integrate trajectory until returning to initial phi,
     then colorize plot of trajectory + period times
     '''
+    if get_y0 is None:
+        get_y0 = lambda q, phi, eta: to_cart(q, phi)
+
     eps_crit = eta * np.sin(I) / (1 - (eta * np.cos(I))**2)
     eps = eps_mult * eps_crit
     # default central phi = CS2's phi
@@ -53,14 +70,14 @@ def trajs(I, eta, eps_mult=0, tf=300, n_pts=101, n_t=101, phi_c=None):
     t_lengths = []
     plots = []
     for q in np.linspace(0, np.pi, n_pts + 2)[1: -1]:
-        y0 = to_cart(q, phi_c)
-        events = [lambda t, y: to_ang(*y0)[1] - to_ang(*y)[1]]
+        y0 = get_y0(q, phi_c, eta)
+        events = [lambda t, y: to_ang(*y0[ :3])[1] - to_ang(*y[ :3])[1]]
         events[0].direction = +1
         ret = solve_ivp(dydt, [0, tf], y0,
                         events=events, dense_output=True)
 
         t_vals = np.linspace(ret.t_events[0][0], ret.t_events[0][1], n_t)
-        q, phi = to_ang(*ret.sol(t_vals))
+        q, phi = to_ang(*ret.sol(t_vals)[ :3])
         plots.append((q, np.unwrap(phi)))
         t_lengths.append(ret.t_events[0][1] - ret.t_events[0][0])
     norm = cm.colors.Normalize(vmin=min(t_lengths), vmax=max(t_lengths))
@@ -85,8 +102,8 @@ def trajs(I, eta, eps_mult=0, tf=300, n_pts=101, n_t=101, phi_c=None):
     eps_str = '0' if eps == 0 else r'10^{%.2f}' % np.log10(eps)
     plt.title(r'$I = %d^\circ, \eta = %.2f, \epsilon = %s (%.2f \epsilon_c)$' %
               (np.degrees(I), eta, eps_str, eps_mult))
-    plt.savefig('3trajs_%s.png' %
-                ('0_00' if eps_mult == 0 else
+    plt.savefig('3%s_%s.png' %
+                (fn_base, '0_00' if eps_mult == 0 else
                  ('%.2f' % eps_mult).replace('.','_')))
     plt.clf()
 
@@ -94,9 +111,14 @@ if __name__ == '__main__':
     I = np.radians(5)
     eta = 0.15
 
-    trajs(I, eta, eps_mult=1.05)
-    trajs(I, eta, eps_mult=0.95)
-    trajs(I, eta, eps_mult=0.45)
-    trajs(I, eta, eps_mult=0.2)
-    trajs(I, eta, eps_mult=0.05)
-    trajs(I, eta)
+    # trajs(I, eta, eps_mult=1.05)
+    # trajs(I, eta, eps_mult=0.95)
+    # trajs(I, eta, eps_mult=0.45)
+    # trajs(I, eta, eps_mult=0.2)
+    # trajs(I, eta, eps_mult=0.05)
+    # trajs(I, eta)
+
+    # try for eta
+    get_y0 = lambda q, phi, eta: [*to_cart(q, phi), eta]
+    trajs(I, eta, get_dydt=get_dydt_eta, get_y0=get_y0, eps_mult=0.4,
+          phi_c=np.pi, fn_base='etatraj')
